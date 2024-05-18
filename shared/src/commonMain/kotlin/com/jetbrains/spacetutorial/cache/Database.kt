bde29c5da250f2e9e5db8e6324622348a105c4da
csldeny <sldenycom@gmail.com>
2024-05-17 20:32:32 -0600

    Implementacion del API service y construccion del SDK

---
+++ shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/cache/Database.kt
@@ -0,0 +1,47 @@
+package com.jetbrains.spacetutorial.cache
+
+import com.jetbrains.spacetutorial.entity.Links
+import com.jetbrains.spacetutorial.entity.Patch
+import com.jetbrains.spacetutorial.entity.RocketLaunch
+
+internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = AppDatabase(databaseDriverFactory.createDriver())
+    private val dbQuery = database.appDatabaseQueries
+
+    internal fun clearAndCreateLaunches(launches: List<RocketLaunch>) {
+        dbQuery.transaction {
+            dbQuery.removeAllLaunches()
+            launches.forEach { launch ->
+                dbQuery.insertLaunch(
+                    flightNumber = launch.flightNumber.toLong(),
+                    missionName = launch.missionName,
+                    details = launch.details,
+                    launchSuccess = launch.launchSuccess ?: false,
+                    launchDateUTC = launch.launchDateUTC,
+                    patchUrlSmall = launch.links.patch?.small,
+                    patchUrlLarge = launch.links.patch?.large,
+                    articleUrl = launch.links.article
+                )
+            }
+        }
+    }
+
+    internal fun clearAndCreateLaunches(launches: List<RocketLaunch>) {
+        dbQuery.transaction {
+            dbQuery.removeAllLaunches()
+            launches.forEach { launch ->
+                dbQuery.insertLaunch(
+                    flightNumber = launch.flightNumber.toLong(),
+                    missionName = launch.missionName,
+                    details = launch.details,
+                    launchSuccess = launch.launchSuccess ?: false,
+                    launchDateUTC = launch.launchDateUTC,
+                    patchUrlSmall = launch.links.patch?.small,
+                    patchUrlLarge = launch.links.patch?.large,
+                    articleUrl = launch.links.article
+                )
+            }
+        }
+    }
+
+}
